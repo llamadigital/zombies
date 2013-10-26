19150edf8349e9aa009aeeb22217278cab31acd9
Chris <chris@clear-carbon.co.uk>
2013-10-26 17:39:39 +0100

    app config update

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Zombies
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.secret_key_base = '2dbcfc8e115015da4763f8e2ce68b3e30713b9555a265e8dc10997535cb86ec53467e11e1f627aa892028a5c558e1d4a79bc23dd7facf6da13357fb22e5b6af0'
   end
 end
